7795e6200ec71bbfccd46013bd932b9e29770410
Charan Romi <charanromi325@gmail.com>
2024-10-18 06:28:50 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 import './App.css';
 
 function App() {
+
+  const [animateMarineLife, setAnimateMarineLife] = useState(false);
+
+  const handleClick = () => {
+    setAnimateMarineLife(true);
+  }
+
+  const fishStyles = useSpring({
+    from: {
+      x: -100,
+    },
+    to: {
+      x: animateMarineLife ? 500 : -100,
+    },
+    config: {
+      duration: 4000,
+    },
+   loop: {
+     reverse: true,
+   }
+  });
+
+  const bubbleStyles = useSpring({
+    from: {
+      y: 0, opacity: 1,
+    },
+    to: {
+      y: animateMarineLife ? -300 : -0, opacity: animateMarineLife ? 0 : 1,
+    },
+    config: {
+      duration: 5000,
+    },
+    loop: true,
+    reset: true,
+  });
+
+
+  const weedStyles = useSpring({
+    from: {
+      rotate: 0,
+    },
+    to: {
+      rotate: animateMarineLife ? 15 : 0,
+    },
+    config: {
+      duration: 3000,
+    },
+    loop: {
+      reverse: true
+    },
+    
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='ocean' onClick={handleClick}>
+        <animated.div className="fish" style={fishStyles}>
+          <img src="/images/clownfish.png" alt='fish' className='fish-image'/>
+        </animated.div>
+
+        <animated.div className="bubble" style={bubbleStyles}>
+          <img src="/images/bubbles.png" alt='bubble' className='bubble-image'/>
+        </animated.div>
+
+        <animated.div className="weed" style={weedStyles}>
+          <img src="/images/seaweed.png" alt='weed' className='weed-image'/>
+        </animated.div>
+
+      </div>
     </div>
   );
 }
